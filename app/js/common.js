e69331e22f811870679db4ae55268ffffbb29d2a
wendivue <vovas2345@gmail.com>
2019-11-03 00:43:31 +1000

    upgrade responsive

--- app/js/common.js
+++ app/js/common.js
@@ -19,6 +19,15 @@ $(document).ready(function(){
       "<svg aria-hidden='true' focusable='false' data-prefix='fas' data-icon='angle-left' class='svg-inline--fa fa-angle-left fa-w-8' role='img' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 256 512'><path fill='currentColor' d='M31.7 239l136-136c9.4-9.4 24.6-9.4 33.9 0l22.6 22.6c9.4 9.4 9.4 24.6 0 33.9L127.9 256l96.4 96.4c9.4 9.4 9.4 24.6 0 33.9L201.7 409c-9.4 9.4-24.6 9.4-33.9 0l-136-136c-9.5-9.4-9.5-24.6-.1-34z'></path></svg>",
       "<svg aria-hidden='true' focusable='false' data-prefix='fas' data-icon='angle-left' class='svg-inline--fa fa-angle-left fa-w-8' role='img' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 256 512'><path fill='currentColor' d='M31.7 239l136-136c9.4-9.4 24.6-9.4 33.9 0l22.6 22.6c9.4 9.4 9.4 24.6 0 33.9L127.9 256l96.4 96.4c9.4 9.4 9.4 24.6 0 33.9L201.7 409c-9.4 9.4-24.6 9.4-33.9 0l-136-136c-9.5-9.4-9.5-24.6-.1-34z'></path></svg>"
     ],
+    responsiveClass:true,
+    responsive:{
+      0:{
+        nav:false
+    },
+      992:{
+        nav:true
+      }
+  },
     autoplay:true,
     autoplayTimeout:6000,
     autoplayHoverPause:true
@@ -43,7 +52,8 @@ $(document).ready(function(){
             items:1
         },
         768:{
-            items:2
+            items:2,
+            nav:false
         },
         992:{
             items:3
